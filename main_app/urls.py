a39e47edad82ee508f62c9c506138747e66881de
James Sullivan <jsullivan2847@gmail.com>
2022-07-09 15:21:51 -0400

    You can create, delete and view dogs

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns  = [
+    #dogs
     path('', views.dogs, name = 'index'),
     path('<int:dog_id>/', views.dog_details, name='details'),
     path('<int:pk>/delete', views.DogDelete.as_view(), name = 'delete_dog'),
     path('<int:pk>/update/', views.DogUpdate.as_view(), name = 'update_dog'),
     path('add_dog', views.DogCreate.as_view(), name = 'create_dog'),
+    path('<int:dog_id>/add_feeding', views.add_feeding, name = 'add_feeding'),
+    #registration
+    path('accounts/signup/', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from .models import Dog
 from .forms import FeedingForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+        
 
 def dogs(request):
     dogs = Dog.objects.all()
@@ -14,6 +24,14 @@ def dog_details(request, dog_id):
     feeding_form = FeedingForm()
     return render(request, 'dogs/details.html', {
         'dog':dog, 'feeding_form': feeding_form })
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+    return redirect('detais', dog_id = dog_id)
 
 class DogCreate(CreateView):
     model = Dog
